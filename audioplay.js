f726a0115838e6ce50849c6ce37e7ae5cc23a38e
Tionlab <tionlab02@gmail.com>
2023-08-28 02:53:31 +0900

    dsasd

--- audioplay.js
+++ audioplay.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
     var audiofile = window.audiofile
     
@@ -8,15 +7,4 @@ document.addEventListener("DOMContentLoaded", function() {
     audioElement.querySelector("source").src = audioFile;
     audioElement.load();
     audioElement.play();
-=======
-document.addEventListener("DOMContentLoaded", function() {
-    var audiofile = window.audiofile
-    
-    const audioElement = document.getElementById("audioSrc");
-    const audioFile = audiofile;
-    
-    audioElement.querySelector("source").src = audioFile;
-    audioElement.load();
-    audioElement.play();
->>>>>>> 9c3e3af12d9937c71028f50317052b42a9697913
 });
